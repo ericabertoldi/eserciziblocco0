b61a5ca6f8663e27e61146af4fd59208372d1c73
ericabertoldi <erica.bertoldi01@gmail.com>
2020-11-16 21:22:35 +0100

    es 7

---
+++ es0.7/es0.7.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"inserire numero"<<endl;
+    cin>>x;
+     if (x%2==0)
+     cout<<"numero pari"<<endl;
+     else cout<<"numero dispari"<<endl;
+     return 0;
+}
